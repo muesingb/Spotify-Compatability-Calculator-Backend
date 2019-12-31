16eed7872d494bbfa6fbcd84127749f38bee7ffd
Barbara <muesingb@gmail.com>
2019-12-31 16:11:38 -0500

    redirecting back to front end show page once user logs in, runs a json server of users that have used the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,35 @@
 class UsersController < ApplicationController
 
     def index
-      redirect_to '/auth/spotify'
+      @users = User.all
+      render json: @users
+      # redirect_to '/auth/spotify'
     end
+
+    def show
+      @user = User.find(params[:id]) 
+      render json: {user: @user, artists: @user.artists, tracks: @user.tracks}
+    end 
 
     def spotify
       @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-      # User.find_or_create_by(
-      #   email: @spotify_user.email
-      # )
+
+      #Update the user access/refresh_tokens
+        # if @spotify_user.access_token_expired?
+        #   @spotify_user.refresh_access_token
+        # else
+        #   @user.update(
+        #     access_token: auth_params["access_token"],
+        #     refresh_token: auth_params["refresh_token"])
+        # end
       
+      #create user in DB
       User.where(:email => @spotify_user.email).first_or_create do |user|
         user.email = @spotify_user.email
         user.display_name = @spotify_user.display_name
       end
 
+      #save users top artist in DB
       @spotify_user.top_artists(limit: 100, offset: 0, time_range: 'medium_term').each do |artist|
         puts artist.name
         Artist.where(:name => artist.name).first_or_create do |artist| 
@@ -23,6 +38,7 @@ class UsersController < ApplicationController
         end
       end
       
+      #save users top tracks in DB
       @spotify_user.top_tracks(limit: 100, offset: 0, time_range: 'medium_term').each do |track|
         puts track.name
         Track.where(:name => track.name).first_or_create do |track| 
@@ -31,12 +47,16 @@ class UsersController < ApplicationController
         end
       end
   
-      # render json: {"redirect":true,"redirect_url": redirect_url}, status:200
+      # # render json: {"redirect":true,"redirect_url": redirect_url}, status:200
 
-      # success: function(data, success, xhr) {
-      #   window.location.href = data.redirect_url;
-      # }
+      # # success: function(data, success, xhr) {
+      # #   window.location.href = data.redirect_url;
+      # # }
+      @user = User.find_by(email: @spotify_user.email)
+      # render json: @user.display_name
   
-      render "spotify"
+      # render "spotify"
+      # render json: @spotify_user
+      redirect_to "http://localhost:3001/user/#{@user.id}"
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
     has_many :artists
     has_many :tracks
+
+    def access_token_expired?
+        puts (Time.now - self.credentials.expires_at)
+    end
 end
